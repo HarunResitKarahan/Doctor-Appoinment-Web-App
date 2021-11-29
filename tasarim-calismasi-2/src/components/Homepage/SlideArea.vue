85df29559b106fbf7f8200dbe89a98e184741fb9
HarunResitKarahan <harun_artin@hotmail.com>
2021-11-29 19:19:34 +0300

    .

--- tasarim-calismasi-2/src/components/Homepage/SlideArea.vue
+++ tasarim-calismasi-2/src/components/Homepage/SlideArea.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="slide-area">
-      <Navbar />
+      
   </div>
 </template>
 
 <script>
-import Navbar from "@/components/Homepage/Navbar.vue";
 export default {
   name: "SlideArea",
-  components: {
-    Navbar,
-  },
 };
 </script>
--- tasarim-calismasi-2/src/views/Home.vue
+++ tasarim-calismasi-2/src/views/Home.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="home">
+    <Navbar />
     <SlideArea />
   </div>
 </template>
 
 <script>
-
+import Navbar from "@/components/Homepage/Navbar.vue";
 import SlideArea from "@/components/Homepage/SlideArea.vue";
 
 export default {
   name: "Home",
   components: {
+    Navbar,
     SlideArea,
   },
 };
