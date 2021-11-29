378514f19ddbd83c233a354228b84cea0f3732b5
HarunResitKarahan <harun_artin@hotmail.com>
2021-11-30 01:31:03 +0300

    .

---
+++ tasarim-calismasi-2/src/components/Homepage/Clinics.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="clinic">
+    asd
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Clinic"
+};
+</script>
--- tasarim-calismasi-2/src/views/Home.vue
+++ tasarim-calismasi-2/src/views/Home.vue
@@ -2,18 +2,24 @@
   <div class="home">
     <Navbar />
     <SlideArea />
+    <Clinic />
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Homepage/Navbar.vue";
 import SlideArea from "@/components/Homepage/SlideArea.vue";
+import Clinic from "@/components/Homepage/Clinics.vue";
 
 export default {
   name: "Home",
   components: {
     Navbar,
     SlideArea,
+    Clinic,
+  },
+  mounted() {  
+    document.title = "Ana Sayfa";  
   },
 };
 </script>
