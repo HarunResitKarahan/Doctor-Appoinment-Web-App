8144dc10f809fa8f3b6951d64c3eb3f49c7f058d
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-01 15:17:53 +0300

    .

---
+++ tasarimcalismasi2/src/components/Homepage/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar">
+    <div class="logo">
+      <a href="#">HARUN KARAHAN</a>
+    </div>
+    <div class="menu">
+      <a href="#">Ana Sayfa</a>
+      <a href="#">Randevu al</a>
+    </div>
+    <div class="log-in">
+      <a href="#">KAYIT OL / GİRİŞ YAP</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
---
+++ tasarimcalismasi2/src/components/Homepage/SlideArea.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="slide-area">
+      <div class="image">
+          <img style="border-radius: 30px;" src="@/./assets/hospital.png" width="600" height="400" alt="">
+      </div>
+      <div class="appointment-tab">
+          <h2>Doktorunu Seç, </h2>
+          <h2>Randevunu Al</h2>
+          <div class="inputs">
+              <div style="margin-top: 35px" class="location">
+                  <span>Şehir</span>
+                  <select style="margin-bottom: 15px;" name="department" id="department">
+                    <option value="bursa">Bursa</option>
+                    <option value="istanbul">İstanbul</option>
+                    <option value="izmir">İzmir</option>
+                    <option value="ankara">Ankara</option>
+                  </select>
+              </div>
+              <div class="location">
+                  <span>Cinsiyet</span>
+                  <select name="sex" id="sex">
+                    <option value="male">Erkek</option>
+                    <option value="female">Kadın</option>
+                  </select>
+              </div>
+              <div class="location">
+                  <span>Bölüm</span>
+                  <select name="department" id="department">
+                    <option value="göz">GÖZ HASTALIKLARI</option>
+                    <option value="beyin">BEYİN VE SİNİR CERRAHİSİ</option>
+                    <option value="noroloji">NÖROLOJİ</option>
+                    <option value="cerrahi">GENEL CERRAHİ</option>
+                  </select>
+              </div>
+              <div class="search">
+                  <p>Randevu Bul</p>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SlideArea'
+}
+</script>
