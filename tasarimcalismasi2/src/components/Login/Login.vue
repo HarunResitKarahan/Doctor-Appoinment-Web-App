9c566a855f8e7c0e6b7565c9a2fcf4df5445765b
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-03 03:12:05 +0300

    .

--- tasarimcalismasi2/src/components/Login/Login.vue
+++ tasarimcalismasi2/src/components/Login/Login.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="login">
       <div class="login-card">
+          <p class="h2">GİRİŞ YAPIN</p>
+          <div class="username">
+              <span>Kullanıcı Adı: </span>
+              <input type="text">
+          </div>
+          <div class="password">
+              <span>Şifre: </span>
+              <input type="text">
+          </div>
+          <div class="remind-me">
+              <label class="checkbox">
+                  <input type="checkbox"><p style="width: 300px;text-align: left;">Beni Hatırla</p>
+              </label>
+              <div class="forgot-password">
+                  <p>Şifremi Unuttum?</p>
+              </div>
+          </div>
+          <div class="log-in">
+              <p>Giriş Yap</p>
+          </div>
       </div>
   </div>
 </template>
