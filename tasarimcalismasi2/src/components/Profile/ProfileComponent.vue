2fdc8015f3e36dcc434445a4b4550430130f06df
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-08 23:55:13 +0300

    .

--- tasarimcalismasi2/src/components/Profile/ProfileComponent.vue
+++ tasarimcalismasi2/src/components/Profile/ProfileComponent.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="profile-component">
       <div class="profile-info col-4">
-        asd
+        <div class="card-body">
+          <img src="@/assets/malephoto.png" width="150" height="150">
+          <p style="font-weight: 500;">Harun Reşit KARAHAN</p>
+          <p style="color: #757575;
+                    font-size: 13px;
+                    font-weight: normal;
+                    margin-top: 8px;
+                    margin-bottom: 8px;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    white-space: nowrap;"
+                    ><b>TC Kimlik No:</b> 38887030636 </p>
+        <div style="text-align: left;
+                    width: 100%;
+                    margin-top:30px;
+                    border: 0 solid #f0f0f0;
+                    border-bottom-width: 1px;
+                    padding: 15px; ">
+            <p class="h6" style="margin:0">Son Randevular</p>
+        </div>
+        </div>
       </div>
       <div class="booking-history col-8">
         asd
