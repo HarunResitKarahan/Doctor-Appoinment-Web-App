f9cc8fb9b8da6c5f12deb8accf7f5a4c7885e330
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-09 18:28:07 +0300

    .

--- tasarimcalismasi2/src/components/Homepage/Navbar.vue
+++ tasarimcalismasi2/src/components/Homepage/Navbar.vue
@@ -14,19 +14,9 @@
     </template>
     <template v-if="issignin == 'Giriş Başarılı'">
       <div class="log-in">
-          <a href="#">PROFİLE</a>
-          <a @click="signout" href="#" style="border-left-width: 0;">Çıkış</a>
+          <div><router-link to="/profile">PROFİLE</router-link></div>
+          <div @click="signout"><router-link to="/" style="border-left-width: 0;">ÇIKIŞ</router-link></div>
       </div>
-      <!-- <div style="margin-right: 30px;display:flex;justify-content: center;width: 5%;">
-       <a href="#" class="dropdown">
-              <b-icon-person id="btn" style="border: 1px solid rgb(71, 70, 71);color: rgb(71, 70, 71);border-radius: 100%;height: 40px;width:40px;padding: 3px;" class="person"/>
-              <b-icon-caret-down-fill style="color: rgb(71, 70, 71);height: 10px;width:10px;vertical-align: middle !important;" class="chevron-down"/>
-              <div class="dropdown-content" align="left">
-                <router-link to="/profile" style="display: none;" :style="issignin === 'Giriş Başarılı' ? 'display: block' : ''">Profile</router-link>
-                <a @click="signout" href="#" :style="issignin === 'Giriş Başarılı' ? '' : 'display: none'">Çıkış</a>
-              </div>
-            </a>
-      </div> -->
     </template>
   </div>
 </template>
