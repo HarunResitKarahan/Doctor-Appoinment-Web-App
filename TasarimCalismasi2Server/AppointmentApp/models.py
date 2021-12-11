358cd693e1c475925273565d3c43aaa014a9cc97
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-11 14:13:04 +0300

    .

--- TasarimCalismasi2Server/AppointmentApp/models.py
+++ TasarimCalismasi2Server/AppointmentApp/models.py
@@ -9,3 +9,30 @@ class Patient(models.Model):
     patientPassword = models.CharField(max_length=200)
     patientCreateTime = models.DateTimeField(default=timezone.now)
     
+class Departman(models.Model):
+    departmanID = models.AutoField(primary_key=True)
+    departmanName = models.CharField(max_length=200)
+
+class City(models.Model):
+    cityID = models.AutoField(primary_key=True)
+    cityName = models.CharField(max_length=200)
+
+class Hospital(models.Model):
+    hospitalID = models.AutoField(primary_key=True)
+    hospitalName = models.CharField(max_length=200)
+    hospitalLocation = models.CharField(max_length=200)
+    hospitalPhoneNumber = models.CharField(max_length=200)
+    hospitalCity = models.ForeignKey(City, default= None, on_delete=models.CASCADE)
+
+class Doctor(models.Model):
+    doctorID = models.AutoField(primary_key=True)
+    doctorName = models.CharField(max_length=200)
+    doctorSurname = models.CharField(max_length=200)
+    doctorScore = models.CharField(max_length=200)
+    departmanID = models.ForeignKey(Departman, default= None, on_delete=models.CASCADE)
+    hospitalID = models.ForeignKey(Hospital, default= None, on_delete=models.CASCADE)
+    doctorCreateTime = models.DateTimeField(default=timezone.now)
+
+class Recommendation(models.Model):
+    recommendationPatientID = models.ForeignKey(Patient, default= None, on_delete=models.CASCADE)
+    recommendationDoctorID =  models.ForeignKey(Doctor, default= None, on_delete=models.CASCADE)
