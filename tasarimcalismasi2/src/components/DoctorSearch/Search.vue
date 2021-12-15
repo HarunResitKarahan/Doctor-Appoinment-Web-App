ad606ca3f7d188df06d2e89be30448d15bf7e480
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-15 19:06:50 +0300

    .

--- tasarimcalismasi2/src/components/DoctorSearch/Search.vue
+++ tasarimcalismasi2/src/components/DoctorSearch/Search.vue
@@ -4,13 +4,21 @@
         <h5>Filtreli Arama</h5>
         <div class="card-body">
           <div class="selected-options">
-            <div class="cards"><span class="material-icons" style="font-size: 14px;border: 1px solid white;cursor: pointer;border-radius: 100%;margin-right: 5px;">close</span><p>{{location}}</p></div>
+            <template v-if="location != undefined">
+              <div class="cards"><span @click="hide($event, 'location')" class="material-icons" style="font-size: 14px;border: 1px solid white;cursor: pointer;border-radius: 100%;margin-right: 5px;">close</span><p>{{location}}</p></div>
+            </template>
+            <template v-if="gender != undefined">
+              <div class="cards"><span @click="hide($event, 'gender')" class="material-icons" style="font-size: 14px;border: 1px solid white;cursor: pointer;border-radius: 100%;margin-right: 5px;">close</span><p>{{gender}}</p></div>
+            </template>
+            <template v-if="department != undefined">
+            <div class="cards"><span @click="hide($event, 'department')" class="material-icons" style="font-size: 14px;border: 1px solid white;cursor: pointer;border-radius: 100%;margin-right: 5px;">close</span><p>{{department}}</p></div>
+            </template>
           </div>
             <input type="date" id="date">
               <h6 style="font-size: 15px;margin-top: 20px;">Şehir</h6>
             <div style="height: 150px;overflow: auto;">
-              <template v-for="item in city">
-                <label class="checkbox" :key="item">
+              <template v-for="(item,index) in city">
+                <label class="checkbox" :key="index">
                       <input @click="onlyOneForLocation($event)" type="checkbox" name="checklocation" :checked="location == item.cityName ? 'checked' : false"><p style="text-align: left;">{{item.cityName}}</p>
                 </label>
               </template>
@@ -26,8 +34,8 @@
             </div>
             <div>
               <h6 style="font-size: 15px;margin-top: 20px;">Poliklinik</h6>
-              <template v-for="item in policlinics">
-                <label class="checkbox" :key="item">
+              <template v-for="(item,index) in policlinics">
+                <label class="checkbox" :key="index">
                   <input @click="onlyOneForClinics($event)" type="checkbox" name="checkclinics" :checked="department == item.departmanName ? 'checked' : false"><p style="text-align: left;">{{item.departmanName}}</p>
                 </label>
               </template>
@@ -78,30 +86,32 @@ export default {
       checkboxgender.forEach((item) => {
         if (item !== event.target) item.checked = false
       })
-      // this.location = event.target.lastChild.textContent
-      // var checkboxes = document.getElementsByName('checklocation')
-      // console.log(checkboxes[0].firstChild)
-      // console.log(checkboxes[0].lastChild)
-      // console.log(event.target)
-      // console.log('------------------------------------------')
-      // checkboxes.forEach((item) => {
-      //   if (item === event.target || item.firstChild === event.target || item.lastChild === event.target) {
-      //     item.firstChild.checked = true
-      //   } else {
-      //     item.firstChild.checked = false
-      //   }
-      // })
     },
     onlyOneForgender: function (event) {
+      this.gender = event.target.parentElement.lastChild.textContent
       var checkboxgender = document.getElementsByName('checkgender')
       checkboxgender.forEach((item) => {
         if (item !== event.target) item.checked = false
       })
     },
     onlyOneForClinics (event) {
+      this.department = event.target.parentElement.lastChild.textContent
       var checkboxes = document.getElementsByName('checkclinics')
       checkboxes.forEach((item) => {
         if (item !== event.target) item.checked = false
+      })
+    },
+    hide (event, prop) {
+      if (prop === 'location') {
+        this.location = undefined
+      } else if (prop === 'gender') {
+        this.gender = undefined
+      } else {
+        this.department = undefined
+      }
+      var checkboxes = document.getElementsByClassName('checkbox')
+      checkboxes.forEach((item) => {
+        if (event.target.parentElement.lastChild.textContent === item.lastChild.textContent) item.firstChild.checked = false
       })
     }
   }
