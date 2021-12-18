dd31a75046766b087dc4a7fb46a3923e7d8c43b3
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-18 17:32:55 +0300

    .

--- tasarimcalismasi2/src/components/DoctorSearch/ShowDoctors.vue
+++ tasarimcalismasi2/src/components/DoctorSearch/ShowDoctors.vue
@@ -14,29 +14,29 @@
           <p style="color: #2E3842;font-size: 20px;font-weight: 500;">{{item.doctorName}} {{item.doctorSurname}}</p>
           <p style="font-size: 14px;color: #757575;margin-bottom: 5px;">{{department}}</p>
           <div class="doctor-rank" style="margin: 0 0 7px 0">
-                <div class="star">
-                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" fill="url(#grad)" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-                    <linearGradient id="grad">
+                <div class="star" :id="item.doctorID">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}a)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}a`">
                         <stop offset="0%" stop-color="white"/>
                         <stop offset="100%" stop-color="white"/>
                     </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" fill="url(#grad1)" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-                    <linearGradient id="grad1">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}1)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}1`">
                         <stop offset="0%" stop-color="white"/>
                         <stop offset="100%" stop-color="white"/>
                     </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" fill="url(#grad2)" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-                    <linearGradient id="grad2">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}2)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}2`">
                         <stop offset="0%" stop-color="white"/>
                         <stop offset="100%" stop-color="white"/>
                     </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" fill="url(#grad3)" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-                    <linearGradient id="grad3">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}3)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}3`">
                         <stop offset="0%" stop-color="white"/>
                         <stop offset="100%" stop-color="white"/>
                     </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" fill="url(#grad4)" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
-                    <linearGradient id="grad4">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}4)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}4`">
                         <stop offset="0%" stop-color="white"/>
                         <stop offset="100%" stop-color="white"/>
                     </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg><p class="after">{{item.doctorScore}}</p><p style="height: 22px;">154 Değerlendirme</p>
@@ -80,21 +80,39 @@ export default {
   methods: {
     star () {
       var doctor = this.doctor
+      var getstar
+      var star
       $('linearGradient').ready(function () {
-        var star = document.querySelectorAll('linearGradient stop')
         doctor.forEach(item => {
+          getstar = document.getElementById(String(item.doctorID))
+          console.log(getstar)
+          star = getstar.querySelectorAll('linearGradient stop')
+          console.log(star)
           console.log(Number(item.doctorScore))
           if (Number(item.doctorScore) > 1 && Number(item.doctorScore) < 2) {
             console.log('girdi 1 - 2 arası')
             star[0].attributes[1].value = 'rgb(255, 185, 88)'
-            console.log(star[0].attributes[1].value = 'rgb(255, 185, 88)')
             star[1].attributes[1].value = 'rgb(255, 185, 88)'
+            star[2].attributes[1].value = 'white'
+            star[3].attributes[1].value = 'white'
+            star[4].attributes[1].value = 'white'
+            star[5].attributes[1].value = 'white'
+            star[6].attributes[1].value = 'white'
+            star[7].attributes[1].value = 'white'
+            star[8].attributes[1].value = 'white'
+            star[9].attributes[1].value = 'white'
           } else if (Number(item.doctorScore) > 2 && Number(item.doctorScore) < 3) {
             console.log('girdi 2 - 3 arası')
             star[0].attributes[1].value = 'rgb(255, 185, 88)'
             star[1].attributes[1].value = 'rgb(255, 185, 88)'
             star[2].attributes[1].value = 'rgb(255, 185, 88)'
             star[3].attributes[1].value = 'rgb(255, 185, 88)'
+            star[4].attributes[1].value = 'white'
+            star[5].attributes[1].value = 'white'
+            star[6].attributes[1].value = 'white'
+            star[7].attributes[1].value = 'white'
+            star[8].attributes[1].value = 'white'
+            star[9].attributes[1].value = 'white'
           } else if (Number(item.doctorScore) > 3 && Number(item.doctorScore) < 4) {
             console.log('girdi 3 - 4 arası')
             star[0].attributes[1].value = 'rgb(255, 185, 88)'
@@ -117,6 +135,8 @@ export default {
             star[5].attributes[1].value = 'rgb(255, 185, 88)'
             star[6].attributes[1].value = 'rgb(255, 185, 88)'
             star[7].attributes[1].value = 'rgb(255, 185, 88)'
+            star[8].attributes[1].value = 'white'
+            star[9].attributes[1].value = 'white'
           }
         })
       })
