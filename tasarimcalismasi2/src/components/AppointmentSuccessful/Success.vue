e9374136ab8b0969083307a32aada036c1de01d8
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-20 23:54:34 +0300

    .

--- tasarimcalismasi2/src/components/AppointmentSuccessful/Success.vue
+++ tasarimcalismasi2/src/components/AppointmentSuccessful/Success.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="success">
-    <div class="verify"><span class="material-icons">verified</span></div>
-    <div class="appointment-info">
-        <p class="successfull"><i>BAŞARIYLA RANDEVU ALINDI</i><span class="material-icons">check</span></p>
-        <div class="successfull-info">
-            <p><i>Randevu Bilgileri: </i></p>
-            <ul>
-                <li><i><strong>Tarih:</strong></i><i> 20 Aralık 2021 9:30</i></li>
-                <li><i><strong>Doktor:</strong></i><i> Enes Demir</i></li>
-                <li><i><strong>Bölüm:</strong></i><i> Dahiliye</i></li>
-                <li><i><strong>Hastane:</strong></i><i> Şevket Yılmaz Devlet Hastanesi</i></li>
-            </ul>
+  <div class="success-comp">
+      <div class="success">
+        <div class="verify"><span class="material-icons">verified</span></div>
+        <div class="appointment-info">
+            <p class="successfull"><i>BAŞARIYLA RANDEVU ALINDI</i><span class="material-icons">check</span></p>
+            <div class="successfull-info">
+                <p><i>Randevu Bilgileri: </i></p>
+                <ul>
+                    <li><i><strong>Tarih:</strong></i><i> 20 Aralık 2021 9:30</i></li>
+                    <li><i><strong>Doktor:</strong></i><i> Enes Demir</i></li>
+                    <li><i><strong>Bölüm:</strong></i><i> Dahiliye</i></li>
+                    <li><i><strong>Hastane:</strong></i><i> Şevket Yılmaz Devlet Hastanesi</i></li>
+                </ul>
+            </div>
         </div>
+    </div>
+    <div>
+        asd
     </div>
   </div>
 </template>
