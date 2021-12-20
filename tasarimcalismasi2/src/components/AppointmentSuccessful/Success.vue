4523f9f7f5d3135d43cd935d3ffa84c37256545a
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-20 20:02:50 +0300

    .

--- tasarimcalismasi2/src/components/AppointmentSuccessful/Success.vue
+++ tasarimcalismasi2/src/components/AppointmentSuccessful/Success.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="success">
-    asd
+    <div class="verify"><span class="material-icons">verified</span></div>
+    <div class="appointment-info">
+        <p class="successfull">BAŞARIYLA RANDEVU ALINDI<span class="material-icons">check</span></p>
+        <div class="successfull-info">
+            <p>Randevu Bilgileri: </p>
+            <ul>
+                <li>asd</li>
+            </ul>
+        </div>
+    </div>
   </div>
 </template>
 
