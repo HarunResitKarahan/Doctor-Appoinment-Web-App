aef640abc3e84a1fe56315234c20ce29a7f5a79a
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-25 00:14:07 +0300

    .

--- tasarimcalismasi2/src/components/AppointmentSuccessful/Success.vue
+++ tasarimcalismasi2/src/components/AppointmentSuccessful/Success.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="success-comp" v-if="success == 'Added Successfully'">
+  <div class="success-comp">
+    <template v-if="success == 'Added Successfully'">
       <div class="success">
         <div class="verify"><span class="material-icons">verified</span></div>
         <div class="appointment-info">
@@ -14,10 +15,31 @@
                 </ul>
             </div>
         </div>
-    </div>
-    <div>
-       <router-link to="/"><p class="return-homepage">Ana Sayfa'ya Dön</p></router-link>
-    </div>
+      </div>
+      <div>
+        <router-link to="/"><p class="return-homepage">Ana Sayfa'ya Dön</p></router-link>
+      </div>
+    </template>
+    <template v-else>
+      <div class="success">
+        <div class="verify"><span class="material-icons">verified</span></div>
+        <div class="appointment-info">
+            <p class="successfull"><i>BAŞARIYLA RANDEVU ALINDI</i><span class="material-icons">new_releases</span></p>
+            <div class="successfull-info">
+                <p><i>Randevu Bilgileri: </i></p>
+                <ul>
+                    <li><i><strong>Tarih:</strong></i><i> {{this.$route.params.dateday}} {{months[this.$route.params.datemonth - 1]}} {{this.$route.params.dateyear}} {{this.$route.params.selectedtime}}</i></li>
+                    <li><i><strong>Doktor:</strong></i><i> {{this.$route.params.doctor[0].doctorName}} {{this.$route.params.doctor[0].doctorSurname}}</i></li>
+                    <li><i><strong>Bölüm:</strong></i><i> {{this.$route.params.department}}</i></li>
+                    <li><i><strong>Hastane:</strong></i><i> {{this.$route.params.hospitalName}}</i></li>
+                </ul>
+            </div>
+        </div>
+      </div>
+      <div>
+        <router-link to="/"><p class="return-homepage">Ana Sayfa'ya Dön</p></router-link>
+      </div>
+    </template>
   </div>
 </template>
 
