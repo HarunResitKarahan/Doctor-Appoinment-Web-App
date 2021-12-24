a423c19ff43f2fb2850f09319a0cda2972cd5c57
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-25 01:39:31 +0300

    .

--- tasarimcalismasi2/src/components/AppointmentSuccessful/Success.vue
+++ tasarimcalismasi2/src/components/AppointmentSuccessful/Success.vue
@@ -22,22 +22,13 @@
     </template>
     <template v-else>
       <div class="success">
-        <div class="verify"><span class="material-icons">verified</span></div>
+        <div class="verify"><span class="material-icons" style="color: #ff8282;">new_releases</span></div>
         <div class="appointment-info">
-            <p class="successfull"><i>BAŞARIYLA RANDEVU ALINDI</i><span class="material-icons">new_releases</span></p>
-            <div class="successfull-info">
-                <p><i>Randevu Bilgileri: </i></p>
-                <ul>
-                    <li><i><strong>Tarih:</strong></i><i> {{this.$route.params.dateday}} {{months[this.$route.params.datemonth - 1]}} {{this.$route.params.dateyear}} {{this.$route.params.selectedtime}}</i></li>
-                    <li><i><strong>Doktor:</strong></i><i> {{this.$route.params.doctor[0].doctorName}} {{this.$route.params.doctor[0].doctorSurname}}</i></li>
-                    <li><i><strong>Bölüm:</strong></i><i> {{this.$route.params.department}}</i></li>
-                    <li><i><strong>Hastane:</strong></i><i> {{this.$route.params.hospitalName}}</i></li>
-                </ul>
-            </div>
+            <p class="successfull"><i>ZATEN RANDEVUNUZ VAR!</i></p>
         </div>
       </div>
       <div>
-        <router-link to="/"><p class="return-homepage">Ana Sayfa'ya Dön</p></router-link>
+        <router-link to="/"><p class="return-homepage" style="background-color: #ff8282;">Ana Sayfa'ya Dön</p></router-link>
       </div>
     </template>
   </div>
