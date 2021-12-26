8108839442c026fd26d7fc1a9d2fd43547ccb2d0
HarunResitKarahan <harun_artin@hotmail.com>
2021-12-27 00:42:53 +0300

    .

--- tasarimcalismasi2/src/components/DoctorSearch/ShowDoctors.vue
+++ tasarimcalismasi2/src/components/DoctorSearch/ShowDoctors.vue
@@ -70,8 +70,72 @@
       <p>Daha Fazla Görüntüle</p>
     </div>
     <VueSlickCarousel :arrows="true" :speed="500" :variableWidth="true" :infinite="false" :slidesToScroll="3" :swipeToSlide="true" style="width: 80%;">
-      <div>asd</div>
-      <div>asd</div>
+      <template v-for="item in doctor">
+      <div class="suggestion-doctor-card" :key="item">
+        <div class="doctor-image">
+          <template v-if="item.doctorSex == 'Erkek'">
+            <img src="@/assets/maledoctor.png" width="75" height="75">
+          </template>
+          <template v-else>
+            <img src="@/assets/femaledoctor.png" width="75" height="75">
+          </template>
+        </div>
+        <div class="doctor-info">
+          <p style="color: #2E3842;font-size: 20px;font-weight: 500;">{{item.doctorName}} {{item.doctorSurname}}</p>
+          <p style="font-size: 14px;color: #757575;margin-bottom: 5px;">{{department}}</p>
+          <div class="doctor-rank" style="margin: 0 0 7px 0">
+                <div class="star" :id="item.doctorID">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}a)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}a`">
+                        <stop offset="0%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                        <stop offset="60%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                    </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}1)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}1`">
+                        <stop offset="0%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                        <stop offset="60%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                    </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}2)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}2`">
+                        <stop offset="0%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                        <stop offset="60%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                    </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}3)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}3`">
+                        <stop offset="0%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                        <stop offset="60%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                    </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="margin: 0 -5px 0 0;padding: 1px;padding-bottom: 3px;font-size: 24px;" width="24" height="24" viewBox="0 0 24 24" :fill="`url(#${item.doctorID}4)`" stroke="rgb(255, 185, 88)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-star">
+                    <linearGradient :id="`${item.doctorID}4`">
+                        <stop offset="0%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                        <stop offset="60%" stop-color="white"/>
+                        <stop offset="100%" stop-color="white"/>
+                    </linearGradient><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg><p class="after">{{item.doctorScore}}</p><p style="height: 22px;">154 Değerlendirme</p>
+                </div>
+          </div>
+          <div class="doctor-departmant">
+              <span class="material-icons" style="color: rgb(2550, 0, 0, 0.300); font-size: 24px;padding: 1px; margin-right: 2px;">emergency</span><p style="padding-top: 3.5px;">{{hospitalName}}</p>
+          </div>
+          <div class="make-appointment">
+            <template v-if="issignin != 'Giriş Başarılı'">
+                <p @click="loginPage">Randevu Al</p>
+            </template>
+            <template v-if="issignin == 'Giriş Başarılı'">
+                <router-link class="appointment" :to="{ name: 'Booking', params: { doctorID: item.doctorID, hospitalName: hospitalName, department: department, date: date } }"><p>Randevu Al</p></router-link>
+            </template>
+          </div>
+        </div>
+    </div>
+    </template>
     </VueSlickCarousel>
     <template v-if="doctor.length == 0">
       <div class="no-found-doctor">
