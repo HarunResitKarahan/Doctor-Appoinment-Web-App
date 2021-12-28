b290694bc2323077ea46ebbeb5b17393300fa37c
Harun <harun0836@gmail.com>
2021-12-28 20:26:29 +0300

    .

--- tasarimcalismasi2/src/components/DoctorSearch/ShowDoctors.vue
+++ tasarimcalismasi2/src/components/DoctorSearch/ShowDoctors.vue
@@ -199,6 +199,7 @@ export default {
           star = $(getstar).find('linearGradient stop')
           if (Math.trunc((Number(item.doctorScore) % 1) * 100) <= 35) {
             var text1 = String(Math.trunc((Number(item.doctorScore) % 1) * 100) + 10)
+            console.log(text1)
           } else if (Math.trunc((Number(item.doctorScore) % 1) * 100) >= 75) {
             text1 = String(Math.trunc((Number(item.doctorScore) % 1) * 100) - 10)
           } else {
@@ -206,7 +207,7 @@ export default {
           }
           var text2 = '%'
           var concat = text1.concat(text2)
-          if (Number(item.doctorScore) > 1 && Number(item.doctorScore) < 2) {
+          if (Number(item.doctorScore) >= 1 && Number(item.doctorScore) < 2) {
             star[0].attributes[1].value = 'rgb(255, 185, 88)'
             star[1].attributes[1].value = 'rgb(255, 185, 88)'
             star[3].attributes[0].value = '0%'
@@ -218,17 +219,19 @@ export default {
             star[5].attributes[0].value = concat
             star[5].attributes[1].value = 'rgb(255, 185, 88)'
             star[6].attributes[0].value = concat
-          } else if (Number(item.doctorScore) > 2 && Number(item.doctorScore) < 3) {
+          } else if (Number(item.doctorScore) >= 2 && Number(item.doctorScore) < 3) {
             star[0].attributes[1].value = 'rgb(255, 185, 88)'
             star[1].attributes[1].value = 'rgb(255, 185, 88)'
             star[2].attributes[1].value = 'rgb(255, 185, 88)'
             star[3].attributes[1].value = 'rgb(255, 185, 88)'
-            star[4].attributes[0].value = '0%'
             star[4].attributes[1].value = 'rgb(255, 185, 88)'
-            star[5].attributes[0].value = concat
             star[5].attributes[1].value = 'rgb(255, 185, 88)'
-            star[6].attributes[0].value = concat
-          } else if (Number(item.doctorScore) > 3 && Number(item.doctorScore) < 4) {
+            star[6].attributes[1].value = 'rgb(255, 185, 88)'
+            star[7].attributes[1].value = 'rgb(255, 185, 88)'
+            star[8].attributes[0].value = concat
+            star[8].attributes[1].value = 'rgb(255, 185, 88)'
+            star[9].attributes[0].value = concat
+          } else if (Number(item.doctorScore) >= 3 && Number(item.doctorScore) < 4) {
             star[0].attributes[1].value = 'rgb(255, 185, 88)'
             star[1].attributes[1].value = 'rgb(255, 185, 88)'
             star[2].attributes[1].value = 'rgb(255, 185, 88)'
@@ -244,7 +247,7 @@ export default {
             star[12].attributes[0].value = concat
             star[12].attributes[1].value = 'rgb(255, 185, 88)'
             star[13].attributes[0].value = concat
-          } else if (Number(item.doctorScore) > 4 && Number(item.doctorScore) < 5) {
+          } else if (Number(item.doctorScore) >= 4 && Number(item.doctorScore) < 5) {
             star[0].attributes[1].value = 'rgb(255, 185, 88)'
             star[1].attributes[1].value = 'rgb(255, 185, 88)'
             star[2].attributes[1].value = 'rgb(255, 185, 88)'
